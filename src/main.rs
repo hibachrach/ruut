1de71889eac4cca0c8fcd7eaa8542c4e9ef01cb7
Harry Bachrach <harry@docsend.com>
2020-02-08 19:47:18 -0800

    Allow structure input from stdin using flag

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,51 @@
+use exitcode;
 use ruut::parser::ParserError;
 use ruut::prettify;
+use std::io::{self, BufRead};
 use structopt::StructOpt;
-use exitcode;
 
 #[derive(StructOpt)]
 struct Cli {
-    serialized_tree: String,
+    serialized_tree: Option<String>,
+    /// Take input from stdin
+    #[structopt(short = "i", long = "stdin")]
+    take_from_stdin: bool,
 }
 
 fn main() {
     let args = Cli::from_args();
-    match prettify(args.serialized_tree) {
-        Ok(prettified) => println!("{}", prettified),
-        Err(ParserError::EmptyTokenSeqError) => {
-            eprintln!("Error: empty input -- structure must be passed as the first argument");
-            std::process::exit(exitcode::USAGE);
+    let stdin = io::stdin();
+    let serialized_tree = match args.serialized_tree {
+        Some(st) => Some(st),
+        None => {
+            if args.take_from_stdin {
+                match stdin.lock().lines().next() {
+                    Some(Ok(first_line_of_stdin)) => Some(first_line_of_stdin),
+                    _ => None,
+                }
+            } else {
+                None
+            }
         }
-        Err(ParserError::MissingNameError) => {
-            eprintln!("Error: invalid input -- note that every open parenthesis must have a name before it");
-            std::process::exit(exitcode::DATAERR);
-        },
-        Err(ParserError::MultipleRootsError) => {
-            eprintln!("Error: invalid input -- must only have one root in structure");
-            std::process::exit(exitcode::DATAERR);
+    };
+    if let Some(st) = serialized_tree {
+        match prettify(st) {
+            Ok(prettified) => println!("{}", prettified),
+            Err(ParserError::EmptyTokenSeqError) => {
+                eprintln!("Error: empty input -- structure must be passed as the first argument or via stdin");
+                std::process::exit(exitcode::USAGE);
+            }
+            Err(ParserError::MissingNameError) => {
+                eprintln!("Error: invalid input -- note that every open parenthesis must have a name before it");
+                std::process::exit(exitcode::DATAERR);
+            }
+            Err(ParserError::MultipleRootsError) => {
+                eprintln!("Error: invalid input -- must only have one root in structure");
+                std::process::exit(exitcode::DATAERR);
+            }
         }
+    } else {
+        eprintln!("Error: no input -- structure must be passed as the first argument or via stdin");
+        std::process::exit(exitcode::USAGE);
     }
 }
